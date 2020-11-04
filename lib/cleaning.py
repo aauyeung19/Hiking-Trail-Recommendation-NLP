ebab130fe356430b20a9a374d9482667f242b584
Andrew Auyeung <andrew.k.auyeung@gmail.com>
2020-11-04 11:33:45 -0500

    added cleaning of digits and measurements

--- lib/cleaning.py
+++ lib/cleaning.py
@@ -13,12 +13,12 @@ from spacy.lemmatizer import Lemmatizer
 from spacy.lookups import Lookups
 import re
 import pickle
-
+import string
 sp = spacy.load('en_core_web_sm')
 stopwords_list = stopwords.words('english')
 stopwords_list.remove('no')
 stopwords_list.remove('not')
-# tokenize by words 
+
 # remove stop words
 def remove_stopwords(text):
     tokens = word_tokenize(text)
@@ -30,10 +30,19 @@ def remove_stopwords(text):
 def remove_digits(text):
     text = re.sub(r'[^A-Za-z\s]', '', text)
     return text
+
+# lemmatize
 def lemma(text):
     text = sp(text)
     text = [word.lemma_ for word in text]
     text = ' '.join(text)
+    return text
+
+# Remove states/named entities
+def remove_loc(text):
+    sentence = sp(text)
+    filtered_sentence = [word for word in sentence if word.ent_type=='GPE']
+    text = ' '.join(filtered_sentence)
     return text
 
 def clean_corpus(corpus):
@@ -44,19 +53,42 @@ def clean_corpus(corpus):
         doc = doc.lower()
         # remove numbers and punctuation
         doc = remove_digits(doc)
-        # remove stop words
-        doc = remove_stopwords(doc)
         # lemmatize
         doc = lemma(doc)
+        # remove stop words
+        doc = remove_stopwords(doc)
 
         cleaned_corpus.append(doc)
     
     return cleaned_corpus
 
+def convert_rating(rating):
+    first = rating[0]
+    if (first =='N') or (first == '-'):
+        return 0
+    else:
+        return int(first)
+
+def clean_measurements(text):
+    try:
+        text = re.sub(r'[^0-9.\s]', '', text)
+        number = float(text)
+        return number
+    except:
+        return None
+
+
 if __name__ == "__main__":
     conn=psycopg2.connect(database='alltrails', user='postgres', host='127.0.0.1', port= '5432')
-    q = "SELECT hike_id, trail_description FROM hikes;"
+    q = "SELECT * FROM hikes;"
     hikes_df = pd.read_sql_query(q, conn)
     hikes_df['cleaned_descriptions'] = clean_corpus(hikes_df['trail_description'])
-    with open('../src/cleaned_hike_desc.pickle', 'wb') as to_write:
-        pickle.dump(hikes_df, to_write)
+    hikes_df['trail_length'] = hikes_df['trail_length'].apply(clean_measurements)
+    hikes_df['trail_elevation'] = hikes_df['trail_elevation'].apply(clean_measurements)
+    hikes_df.to_csv('../src/clean_all_hikes.csv')
+
+    q = "SELECT * FROM reviews;"
+    reviews_df = pd.read_sql_query(q, conn)
+    reviews_df.dropna(inplace=True)
+    reviews_df['cleaned_reviews'] = clean_corpus(reviews_df['user_desc'])
+    reviews_df.to_csv('../src/clean_reviews.csv')
